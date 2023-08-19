4ec95a803f4c8cf950f4b90dfd1b68d6e9b6db39
Eyosias <eyosiasgezahegn28@gmail.com>
2023-08-19 14:54:42 +0300

    Added AppBar and searchbar.dart

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/searchbar.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,10 +10,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController searchEditingController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text("Home"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Food Recipe",)),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.menu))
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child:Text('Find The Best Recipes\nFor Cooking',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),),
+              ),
+
+              MySearchBar(
+                controller: searchEditingController,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/searchbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MySearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  const MySearchBar({
+    required this.controller,
+    super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey[200], // Light grey color
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: TextField(
+          controller: controller,
+          decoration: const InputDecoration(
+            prefixIcon: Icon(Icons.search_rounded),
+            hintText: 'Search For Recipes',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+          ),
+        ),
+      ),
+    );
+
+
+
+  }
+}
